26f84603234abe9d092d4b1f1c88c57df58f06d2
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-01 18:16:26 +0000

    Script to mirror git repositories (rails.git and merkaartor.git)

---
+++ git-mirror.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~/g/$REPO_NAME
+git pull         >/dev/null
+git fetch --tags >/dev/null
+
+for branch in $(git branch -la | grep remotes/origin/ |grep -v -- '->' | sed 's/^  //'); do
+    local_branch=$(echo $branch | perl -pe 's[^remotes/origin/][]')
+
+    git checkout -b $local_branch $branch 2>/dev/null
+done
+
+git checkout master 2>/dev/null
+
+git push mirror --all  >/dev/null 2>&1
+git push mirror --tags >/dev/null 2>&1
