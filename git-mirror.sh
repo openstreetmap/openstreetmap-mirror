f239e6697ad7841b89cb60c2862aac51a5c73687
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-02 08:42:07 +0000

    git pull only updates the checked out local branch
    Thanks to Jan Krüger in #git

--- git-mirror.sh
+++ git-mirror.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 cd /var/lib/openstreetmap-mirror/$REPO_NAME
-git pull         >/dev/null
-git fetch --tags >/dev/null
 
 for branch in $(git branch -la | grep remotes/origin/ |grep -v -- '->' | sed 's/^  //'); do
     local_branch=$(echo $branch | perl -pe 's[^remotes/origin/][]')
 
     git checkout -b $local_branch $branch 2>/dev/null
+    git checkout $local_branch            2>/dev/null
+    git pull                              >/dev/null
 done
 
+git fetch --tags >/dev/null
 git checkout master 2>/dev/null
 
 git push mirror --all  >/dev/null 2>&1
