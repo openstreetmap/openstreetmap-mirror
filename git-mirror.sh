560da29523d277e67dc607831610b3b689bc8f57
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-11 22:01:40 +0000

    Reverting "Just push bare repos when mirroring Git"
    Didn't work, git fetch on bare repos doesn't update all branches.

--- git-mirror.sh
+++ git-mirror.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 REPO_NAME=$1
-FROM_URL=$2
 
-cd /var/lib/openstreetmap-mirror/$REPO_NAME.git
-git --bare fetch $FROM_URL
-git push git@github.com:openstreetmap/$REPO_NAME.git --all
+cd /var/lib/openstreetmap-mirror/$REPO_NAME
+
+for branch in $(git branch -la | grep remotes/origin/ |grep -v -- '->' | sed 's/^  //'); do
+    local_branch=$(echo $branch | perl -pe 's[^remotes/origin/][]')
+
+    git checkout -b $local_branch $branch 2>/dev/null
+    git checkout $local_branch            2>/dev/null
+    git pull                              >/dev/null
+done
+
+git fetch --tags >/dev/null
+git checkout master 2>/dev/null
+
+git push mirror --all  >/dev/null 2>&1
+git push mirror --tags >/dev/null 2>&1
 
 exit 0
