529cb7fed11c4c490aed4316d5e0fd4bb22702a0
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-09-26 15:37:19 +0000

    josm-mirror.sh: update for changed Apache URL

--- josm-mirror.sh
+++ josm-mirror.sh
@@ -6,10 +6,10 @@ git checkout mirror
 git merge master
 
 # Just do a plain copy of the externals into this repository.
-svn export --force http://svn.apache.org/repos/asf/ant/core/trunk/src/main/org/apache/tools/bzip2               src/org/apache/tools/bzip2   >/dev/null
-svn export --force http://svn.openstreetmap.org/applications/viewer/jmapviewer/src/org/openstreetmap/gui        src/org/openstreetmap/gui    >/dev/null
-svn export --force http://svn.openstreetmap.org/applications/share/map-icons/classic.small                      images/styles/standard       >/dev/null
-svn export --force http://svn.apache.org/repos/asf/commons/proper/codec/trunk/src/java/org/apache/commons/codec src/org/apache/commons/codec >/dev/null
+svn export --force http://svn.apache.org/repos/asf/ant/core/trunk/src/main/org/apache/tools/bzip2                    src/org/apache/tools/bzip2   >/dev/null
+svn export --force http://svn.openstreetmap.org/applications/viewer/jmapviewer/src/org/openstreetmap/gui             src/org/openstreetmap/gui    >/dev/null
+svn export --force http://svn.openstreetmap.org/applications/share/map-icons/classic.small                           images/styles/standard       >/dev/null
+svn export --force http://svn.apache.org/repos/asf/commons/proper/codec/trunk/src/main/java/org/apache/commons/codec src/org/apache/commons/codec >/dev/null
 
 # Commit externals changes, if any
 git config user.name "JOSM GitHub mirror"
