openstreetmap/openstreetmap-mirror

commit f2725334dcd470f91be0d14a61b7f11f40304995

git-mirror.sh: Make this work better with modern Git
Now it has the --mirror option, which does most of this complexity for
us.

git-mirror.sh
@@ -18,6 +18,7 @@ fi
 cd $REPO_NAME
 git remote add origin $REPO_FROM || :
 git remote add mirror $REPO_TO   || :
+git config --bool remote.mirror.mirror true || ::
 
-git fetch
-git push -f mirror refs/remotes/origin/*:refs/heads/*
+git fetch --all
+git push mirror

commit 9b56b1f4b607d9d957904411a4b270494936dbc2

git-mirror.sh: I was missing a refspec to fetch
So the end result was that after the initial clone I ended up fetching
nothing, make sure we update our local branches/tags/whatever before
mirroring.

This had been broken since my f272533 when I started using the
--mirror option.

git-mirror.sh
@@ -18,7 +18,8 @@ fi
 cd $REPO_NAME
 git remote add origin $REPO_FROM || :
 git remote add mirror $REPO_TO   || :
-git config --bool remote.mirror.mirror true || ::
+git config --bool remote.mirror.mirror true || :
+git config remote.origin.fetch '+refs/heads/*:refs/heads/*' || :
 
 git fetch --all
 git push mirror