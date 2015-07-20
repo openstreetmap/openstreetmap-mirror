1bc67815b1b3b7633d453e7498ca87eb6ab3299a
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2015-07-20 14:46:03 +0000

    Another bugfix for how we get external properties
    As noted in the comments for issue #3 I was creating some svn-* files
    here. This won't explicitly solve that, but tries to solve the more
    general problem of files added by the externals not going away.
    
    This still might happen again, so maybe I should start maintaining a
    .gitignore file here.

--- josm-mirror.sh
+++ josm-mirror.sh
@@ -12,7 +12,17 @@ git merge master
 #
 # The format is sort of bizarre and inconsistent, but these two
 # regexes seem to capture the external we need to clone.
-git svn show-externals | grep ^/ | perl -pe 's[^/([^ ]+) (http[^ \n]+)$][svn export --force $2 $1]m; s[^/([^ ]+?)/(http[^ ]+) ([^ ]+)$][svn export --force $2 $1/$3]' | sh
+#
+# The reason for doing the removal here is that if something gets
+# removed from the export we won't remove those files unless we nuke
+# the whole directory and re-export it. This still isn't perfect, if
+# the externals list changes to remove an external we won't remove it,
+# but hopefully that doesn't happen that often..
+tmpexternals=$(mktemp .git/josm-mirror.sh-XXXXX)
+git svn show-externals | grep ^/ | perl -pe 's[^/([^ ]+) (http[^ \n]+)$][svn export --force $2 $1]m; s[^/([^ ]+?)/(http[^ ]+) ([^ ]+)$][svn export --force $2 $1/$3]' >$tmpexternals
+rm -rfv $(awk '{print $5}' $tmpexternals)
+. $tmpexternals
+rm -v $tmpexternals
 
 # Commit externals changes, if any
 git config user.name "JOSM GitHub mirror"
